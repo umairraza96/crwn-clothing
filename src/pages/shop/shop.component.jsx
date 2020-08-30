32c553dcf3cf859a584fba63bb466229850e19a8
Umair Raza <umair.raza666@gmail.com>
2020-08-30 15:16:28 +0500

    added shop page, collection preview component and collection item

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import SHOP_DATA from "./shop.data";
+
+class ShopPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div>
+        {collections.map((collection) => (
+          <CollectionPreview key={collection.id} {...collection} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
