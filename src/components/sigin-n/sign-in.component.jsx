3ef650cfa7bfe7f158500601bfe39be64b0d0840
Umair Raza <umair.raza666@gmail.com>
2020-09-01 11:34:44 +0500

    added Sign In and Sign Up Page, Sign-in,form-input and custom button component

---
+++ src/components/sigin-n/sign-in.component.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import "./sign-in.styles.scss";
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+
+class SignIn extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      email: "",
+      password: "",
+    });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with email and password</span>
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            label="Email"
+            type="email"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
+
+          <FormInput
+            label="Password"
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          />
+          <CustomButton type="submit">Sign In</CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./sign-in-and-sign-up.styles.scss";
+import SignIn from "../../components/sigin-n/sign-in.component";
+
+const SignInAndSignUp = () => (
+  <div>
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUp;
