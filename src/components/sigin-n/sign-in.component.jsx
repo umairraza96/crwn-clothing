4a82c8ec8d0222e2c4bad2885dbbb0b27d063b9c
Umair Raza <umair.raza666@gmail.com>
2020-09-06 18:21:08 +0500

    finishing handle submit method

--- src/components/sigin-n/sign-in.component.jsx
+++ src/components/sigin-n/sign-in.component.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import "./sign-in.styles.scss";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
+// import { auth } from "../../firebase";
 
 class SignIn extends Component {
   constructor() {
@@ -18,12 +19,18 @@ class SignIn extends Component {
     this.setState({ [name]: value });
   };
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({
-      email: "",
-      password: "",
-    });
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        email: "",
+        password: "",
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   render() {
