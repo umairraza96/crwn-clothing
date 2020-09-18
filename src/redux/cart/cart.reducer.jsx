971dddd4d265817c70657738c87664c4904b812f
Umair Raza <umair.raza666@gmail.com>
2020-09-18 22:11:19 +0500

    implemented addItem function in cart reducer.

--- src/redux/cart/cart.reducer.jsx
+++ src/redux/cart/cart.reducer.jsx
@@ -2,6 +2,7 @@ import CartActionTypes from "./cart.types";
 
 const INITIAL_STATE = {
   hidden: true,
+  cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -9,6 +10,9 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return { ...state, hidden: !state.hidden };
 
+    case CartActionTypes.ADD_ITEM:
+      return {...state,cartItems: [...state.cartItems,action.payload]}
+      
     default:
       return state;
   }
