5cdb1655277d0bf9541fac48015cf44137b40a65
Umair Raza <umair.raza666@gmail.com>
2020-09-22 11:08:24 +0500

    updating application with reselect and replaced cart selectors with memoized selectors

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,17 @@
+import { createSelector } from "reselect";
+
+const selectCart = (state) => state.cart;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce(
+      (accumulatedValue, cartItem) => accumulatedValue + cartItem.quantity,
+      0
+    )
+);
