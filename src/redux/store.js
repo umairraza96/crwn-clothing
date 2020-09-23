2e5ca653c4762deea65b423457f537d1a3638505
Umair Raza <umair.raza666@gmail.com>
2020-09-23 14:53:29 +0500

    updated app with react-persist and added the persistance to cart in root-reducer and added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./root-reducer";
 import logger from "redux-logger";
+import { persistStore } from "redux-persist";
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
